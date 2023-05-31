ac76d0995466c20f2aa5506a1cfd7d59fdc6a48e
Diuri <diuri.student@gmail.com>
2023-05-31 14:14:53 -0300

    api instance error v5.0

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import ImagePopup from './ImagePopup.js'
 import EditProfilePopup from './EditProfilePopup.js'
 import EditAvatarPopup from './EditAvatarPopup.js'
 import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
-import api from './api.js'
+import api from '../utils/Api.js'
 
 export default function App() {
 
